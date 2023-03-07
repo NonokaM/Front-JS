6eb528e0935becfaeb257fb43e674de9e47c322f
nonokaM <c1201071@gmail.com>
2023-03-07 23:42:20 +0900

    Chapter3

--- ColorPicker/script.js
+++ ColorPicker/script.js
@@ -1,2 +1,12 @@
-console.log(document.querySelector('#colorPicker').value);
-document.querySelector('#colorText').textContent = `ColorCode:${document.querySelector('#colorPicker').value}`;
+// console.log(document.querySelector('#colorPicker').value);
+// document.querySelector('#colorText').textContent = `ColorCode:${document.querySelector('#colorPicker').value}`;
+
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+// アロー関数
+const colorBg = () => {
+    text.textContent = `カラーコード:${color.value}`;
+}
+
+color.addEventListener('input', colorBg);
